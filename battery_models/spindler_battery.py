d7fb858050b3bb4d06d457ac533b3164ebb158c5
Xiang Gao <xgao4679@usc.edu>
2023-07-27 00:43:19 -0700

    finished refactoring

---
+++ battery_models/spindler_battery.py
@@ -0,0 +1,15 @@
+from abc import ABC
+from datetime import datetime
+
+from battery import Battery
+
+
+class SpindlerBattery(Battery, ABC):
+    def __init__(self, last_service_date, current_date):
+        super().__init__()
+        self.last_service_date,= last_service_date,
+        self.current_date = current_date
+
+    def needs_service(self):
+        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 2)
+        return service_threshold_date < datetime.today().date()
